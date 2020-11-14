89c2cd0ea0345c0a80121cc0ef05425ac545b33f
Misevich Alexander WX5324211 <misevich.alexander@huawei.com>
2020-11-14 17:39:34 +0300

    Add comparison

--- trees/src/applications/demo/main.cpp
+++ trees/src/applications/demo/main.cpp
@@ -1,7 +1,10 @@
+#include <chrono> 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <trees/Tree.h>
+#include <trees/Set.h>
+
 
 int main() 
 { 
@@ -9,6 +12,7 @@ int main()
     std::cout << "Enter number of keys" << std::endl;
 	std::cin >> numOfKeys;
 	trees::SplayTree<int> tree;
+	trees::StandardSet<int> standardTree;
 
     std::cout << "Enter all keys" << std::endl;
     int key = 0;
@@ -16,6 +20,7 @@ int main()
     {
 		std::cin >> key;
 		tree.insert(key);
+		standardTree.insert(key);
 	}
 	
 	size_t numOfQueries = 0;
@@ -24,6 +29,7 @@ int main()
 
     // Results
 	std::vector<size_t> results(numOfQueries, 0u);
+	std::vector<size_t> resultsSet(numOfQueries, 0u);
 	
     std::cout << "Enter all queries" << std::endl;
     int lower = 0;
@@ -32,12 +38,17 @@ int main()
     {
 		std::cin >> lower;
 		std::cin >> upper;
+		auto start = std::chrono::high_resolution_clock::now();
 		results[i] = tree.rangeQuery(lower, upper);
+		std::cout << "Time for SplayTree: " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
+		start = std::chrono::high_resolution_clock::now();
+		resultsSet[i] = standardTree.rangeQuery(lower, upper);
+		std::cout << "Time for std::set: " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
 	}
 	
     std::cout << "Results\n" << std::endl;
 	for(size_t i = 0; i < results.size(); ++i) 
     {
-		std::cout << results[i] << std::endl;
+		std::cout << results[i] << " " << resultsSet[i] << std::endl;
 	}
 }
